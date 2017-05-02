bb913dc2ab1a8ad58cff97c4c41e83b4c299b218
engHossam84 <eng.hossam84@gmail.com>
2017-05-02 19:20:13 +0200

    Add files via upload

---
+++ Prototype.cpp
@@ -0,0 +1,102 @@
+#include <string>
+#include <list>
+#include <iostream>
+using namespace std;
+void ProtoTypeTest();
+class IPerson
+{
+public:
+    virtual IPerson* Clone() = 0;
+	void print()
+	{
+		cout << "Name = " << m_sName << " , ID = " << m_ID << endl;
+	}
+    IPerson(const string& sName, int id):m_sName(sName),m_ID(id) { }
+    IPerson(const IPerson& person) { this->m_sName = person.m_sName;
+                                     this->m_ID      = person.m_ID;
+                                   }
+    void SetName(const string& sName) { m_sName = sName; }
+    void SetID(int ID)                { m_ID = ID;       }
+private:
+    string m_sName;
+    int m_ID;
+};
+ 
+class Student: public IPerson
+{
+public:
+    Student(const string& sName, int id):IPerson(sName, id)
+    {
+ 
+    }
+    Student(const Student& student):IPerson(student)
+    {
+ 
+    }
+
+	
+ 
+    IPerson* Clone()
+    {
+        return new Student(*this);
+    }
+};
+class Teacher: public IPerson
+{
+public:
+    Teacher(const string& sName, int id):IPerson(sName, id)
+    {
+ 
+    }
+ 
+    Teacher(const Teacher& teacher):IPerson(teacher)
+    {
+ 
+    }
+ 
+    IPerson* Clone()
+    {
+        return new Teacher(*this);
+    }
+};
+ 
+class University
+{
+public:
+ 
+    University(const string& sName):m_sName(sName)
+    {
+ 
+    }
+    University(const University& univ):m_sName(univ.m_sName){
+        for (auto it = univ.m_Members.begin(); it != univ.m_Members.end(); it++)
+		{
+			(*it)->print();
+            m_Members.push_back((*it)->Clone());
+		}
+    }
+    void AddMember( IPerson* ptr) { m_Members.push_back(ptr); }
+private:
+    list<IPerson*> m_Members;
+    string m_sName;
+};
+ 
+ 
+void ProtoTypeTest()
+{
+    University* pUniversity = new University("Oxford");
+    IPerson* ptr1 = new Student("Messi",1);
+    IPerson* ptr2 = new Student("Ronaldo",2);
+    IPerson* ptr3 = new Teacher("Scolari",3);
+    pUniversity->AddMember(ptr1);
+    pUniversity->AddMember(ptr2);
+    pUniversity->AddMember(ptr3);
+   // calls copy constructor of University class, which in turn calls Clone()  method of Iperson
+    University* pUniversity2 = new University(*pUniversity);
+ 
+}
+
+void main()
+{
+	ProtoTypeTest();
+}
